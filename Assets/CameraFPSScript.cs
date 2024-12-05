2e41cad4244b30a7556580f934b677c992bbe80c
Francisco Calarge <fjcalarge@gmail.com>
2024-12-05 17:16:42 -0400

    commit fix fulscreen render

--- Assets/CameraFPSScript.cs
+++ Assets/CameraFPSScript.cs
@@ -6,13 +6,16 @@ public class CameraFPSScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward*Input.GetAxis("Vertical")*Time.deltaTime*12;
-        transform.RotateAround(transform.position, transform.up, Input.GetAxis("Horizontal")*Time.deltaTime*60);
+        transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * 12;
+        transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * 12;
+
+        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X")*Time.deltaTime*600);
+
     }
 }
