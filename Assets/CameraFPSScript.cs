b6b2761d0bb12f7b47026a1e3aea206c34eceb37
Francisco Calarge <fjcalarge@gmail.com>
2024-12-10 18:47:01 -0400

    commit kenney assets

--- Assets/CameraFPSScript.cs
+++ Assets/CameraFPSScript.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 
 public class CameraFPSScript : MonoBehaviour
 {
+    private float rotateX, rotateY = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rotateX = 0;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
@@ -15,7 +18,12 @@ public class CameraFPSScript : MonoBehaviour
         transform.position += transform.forward * Input.GetAxis("Vertical") * Time.deltaTime * 12;
         transform.position += transform.right * Input.GetAxis("Horizontal") * Time.deltaTime * 12;
 
-        transform.Rotate(Vector3.up, Input.GetAxis("Mouse X")*Time.deltaTime*600);
+        rotateX+=Input.GetAxis("Mouse Y") *Time.deltaTime*200;
+        rotateX = Mathf.Clamp(rotateX, -50, 50f);
+        rotateY += Input.GetAxis("Mouse X") * Time.deltaTime * 200;
+
+        transform.localEulerAngles = Vector3.right * -rotateX+ Vector3.up * rotateY;
+
 
     }
 }
